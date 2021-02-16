32baf08f32ea74688b92961f590a2e72b1028e3b
subhajit2001 <subhajit.asia@gmail.com>
2021-02-16 11:28:49 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -5,12 +5,38 @@ import App from "./App";
 
 //ReactDOM.render(<App />, document.getElementById("root"));
 
-let counter = 0;
+/*let counter = 0;
 
 function show() {
   counter++;
   const el = <p>{counter}</p>;
   ReactDOM.render(el, document.getElementById("root"));
-}
+}*/
 
-setInterval(show, 1000);
+//Functional Components
+function Hello(){
+  return <h1>Hello World</h1>;
+}
+const el = <Hello />;
+ReactDOM.render(
+  el,
+  document.getElementById('root')
+);
+
+//Class Components
+class Hello1 extends React.Component{
+  render(){
+    return <h1>Hello First time react.</h1>;
+  }
+}
+const el1 = <Hello1 />
+
+ReactDOM.render(
+  el1,
+  document.getElementById('root1')
+);
+
+//props - arguments to Functional Components
+
+
+//setInterval(show, 1000);
