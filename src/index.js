52d1c422c029aee170c3805150cb5cb90deab301
subhajit2001 <subhajit.asia@gmail.com>
2021-02-16 17:24:46 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -37,6 +37,65 @@ ReactDOM.render(
 );
 
 //props - arguments to Functional Components
+function Hello3(props){
+  return <p>Hello, {props.name}!</p>;
+}
+const el3 = <Hello3 name="David"/>;
+ReactDOM.render(
+  el3,
+  document.getElementById('root3')
+);
+
+function App() {
+  return <div>
+    <Hello3 name="David" />
+    <Hello3 name="James" />
+    <Hello3 name="Amy" />
+  </div>;
+}
+const el4 = <App />
+ReactDOM.render(
+  el4,
+  document.getElementById('root4')
+);
+
+class Hello4 extends React.Component{
+  render(){
+    return <p>Hello, {this.props.name}!</p>;
+  }
+}
+const el5 = <Hello4 name="Subhajit"/>
+ReactDOM.render(
+  el5,
+  document.getElementById('root5')
+);
+
+//name and price props
+function Item(props){
+  return <div className="item">
+  <b>Name:</b> {props.name}<br/>
+  <b>Price:</b> {props.price}<br/>
+  </div>;
+}
+ReactDOM.render(
+  <Item name="Subhajit" price="156"/>,
+  document.getElementById('root6')
+);
+
+//state attribute of class components
+class Hello5 extends React.Component{
+  state = {
+    name:"James"
+  }
+  render(){
+   return <h1>Hello {this.state.name}.</h1> 
+  }
+}
+ReactDOM.render(
+  <Hello5 />,
+  document.getElementById('root7')
+);
+
 
 
 //setInterval(show, 1000);
